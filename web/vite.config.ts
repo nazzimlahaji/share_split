682562dc4dfd1bfa4017624e01efe82422f2da11
Nazzim <nazzim@tuxuri.com>
2024-05-24 11:28:10 +0800

    chore: add checker

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig, loadEnv } from "vite";
+import checker from "vite-plugin-checker";
+// import eslint from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+/** @type {import('vite').UserConfig} */
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    // Plugins configuration
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+        eslint: {
+          // for example, lint .ts and .tsx
+          lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+        },
+      }),
+      // eslint({ cache: false }),
+    ],
+
+    // Server configuration
+    server: {
+      port: parseInt(env.VITE_PORT), // Convert port to a number
+      strictPort: true,
+      host: true,
+      // open: true,
+      // proxy: {
+      //   "/api": env.VITE_PROXY_API_URL, // Directly use the environment variable
+      // },
+    },
+  };
+});
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
