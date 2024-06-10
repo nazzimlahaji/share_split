447ebc6d1a4c2578cd536dc737d6d303c6f6a03e
Nazzim <nazzim@tuxuri.com>
2024-06-10 15:19:07 +0800

    chore: Add DeactivatedAt field to Users model

--- api/models/user.go
+++ api/models/user.go
@@ -2,14 +2,16 @@ package models
 
 import (
 	"api/helpers"
+	"time"
 
 	"github.com/google/uuid"
 )
 
 type Users struct {
 	helpers.Model
-	Email  string `gorm:"unique"`
-	Name   string
-	RoleID uuid.UUID `gorm:"not null,constraint:OnUpdate:CASCADE,OnDelete:SET NULL;type:uuid;"`
-	Role   Role      `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	Email         string `gorm:"unique"`
+	Name          string
+	RoleID        uuid.UUID  `gorm:"not null,constraint:OnUpdate:CASCADE,OnDelete:SET NULL;type:uuid;"`
+	Role          Role       `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	DeactivatedAt *time.Time `gorm:"default:null"`
 }
