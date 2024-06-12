50ba5382dfff31dc70f21cb74adeeb47233578bf
Nazzim <nazzim@tuxuri.com>
2024-06-12 11:56:41 +0800

    feat: update userList API to handle pagination

--- api/controllers/userController.go
+++ api/controllers/userController.go
@@ -4,8 +4,10 @@ import (
 	"api/helpers"
 	"api/initializers"
 	"api/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 )
 
 type CreateUserRequest struct {
@@ -14,11 +16,13 @@ type CreateUserRequest struct {
 }
 
 type UserListResponse struct {
-	ID        uint   `json:"id"`
-	Name      string `json:"name"`
-	Email     string `json:"email"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	ID            uuid.UUID `json:"id"`
+	Name          string    `json:"name"`
+	Email         string    `json:"email"`
+	RoleName      string    `json:"role_name"`
+	DeactivatedAt string    `json:"deactivated_at"`
+	CreatedAt     string    `json:"created_at"`
+	UpdatedAt     string    `json:"updated_at"`
 }
 
 func CreateUser(c *gin.Context) {
@@ -57,7 +61,22 @@ func CreateUser(c *gin.Context) {
 
 func UserList(c *gin.Context) {
 	var users []UserListResponse
-	result := initializers.DB.Model(&models.Users{}).Select("id", "email", "name", "created_at", "updated_at").Find(&users)
+
+	// Get page number from query string, default to 1 if it doesn't exist
+	page, _ := strconv.Atoi(c.DefaultQuery("page", "1"))
+	// Get page size from query string, default to 10 if it doesn't exist
+	perPage, _ := strconv.Atoi(c.DefaultQuery("perPage", "10"))
+
+	// Calculate offset
+	offset := (page - 1) * perPage
+
+	result := initializers.DB.Model(&models.Users{}).
+		Select("users.id", "users.email", "users.name", "users.deactivated_at", "users.created_at", "users.updated_at", "roles.name as role_name").
+		Joins("left join roles on users.role_id = roles.id").
+		Offset(offset). // Add offset
+		Limit(perPage). // Limit the number of users
+		Find(&users)
+
 	if result.Error != nil {
 		c.JSON(500, gin.H{
 			"metadata": gin.H{
@@ -70,7 +89,9 @@ func UserList(c *gin.Context) {
 
 	c.JSON(200, gin.H{
 		"metadata": gin.H{
-			"message": "Users fetched successfully",
+			"message":  "Users fetched successfully",
+			"page":     page,
+			"per_page": perPage,
 		},
 		"data": users,
 	})
